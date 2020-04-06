990a02d2f04c481268441bacbfe39347a856a356
Jwilson1172 <jw59615@gmail.com>
2020-04-06 17:04:35 -0400

    made some changes to packaging options and released patched version to test.pypi
    Signed-off-by: Jwilson1172 <jw59615@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-Jwilson1172",
-    version="0.0.2",
+    version="0.0.3",
     author="Joseph Wilson",
     author_email="jw59615@gmail.com",
     description="A package with some simple ds tools for practicing twine syntax",
