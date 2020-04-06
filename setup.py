7750015539b4d93a121e5e84eedfe4a007013056
Jwilson1172 <jw59615@gmail.com>
2020-04-06 16:10:16 -0400

    updating setup.py for packaging version 0.0.2
    Signed-off-by: Jwilson1172 <jw59615@gmail.com>

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-Jwilson1172",
-    version="0.0.1",
+    version="0.0.2",
     author="Joseph Wilson",
     author_email="jw59615@gmail.com",
-    description="A small example package",
+    description="A package with some simple ds tools for practicing twine syntax",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Jwilson1172/lambdata_Jwilson1172",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
